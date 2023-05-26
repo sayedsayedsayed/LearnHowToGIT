78e6c1317fef0ce21a9f6b5efe345ef10dd0dbb6
Sayed Zulfikar <74494579+sayedsayedsayed@users.noreply.github.com>
2023-05-26 15:58:24 +0700

    First Commit List View

--- LearnHowToGIT/ContentView.swift
+++ LearnHowToGIT/ContentView.swift
@@ -7,20 +7,62 @@
 
 import SwiftUI
 
+// Orang ke 1
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+            }
+            .listStyle(.plain)
+            .navigationTitle("Awesome List")
         }
-        .padding()
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListRowView: View {
+    var body: some View {
+        NavigationLink(destination: Text("Laman Orang Kedua")) {
+            HStack(alignment: .top) {
+                Rectangle()
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.blue)
+                
+                VStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: 80, height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                }
+                .foregroundColor(.orange)
+            }
+            .padding(.vertical, 10)
+        }
     }
 }
