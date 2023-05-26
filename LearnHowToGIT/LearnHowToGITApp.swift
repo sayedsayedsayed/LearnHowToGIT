54befc9be37ae4ef1d9b093cf5f979ea7263b7fe
Sayed Zulfikar <74494579+sayedsayedsayed@users.noreply.github.com>
2023-05-26 15:40:03 +0700

    Initial Commit

---
+++ LearnHowToGIT/LearnHowToGITApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnHowToGITApp.swift
+//  LearnHowToGIT
+//
+//  Created by Sayed Zulfikar on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnHowToGITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
